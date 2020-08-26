5fa1c77e497e9e33f15f52a1967bf4e96388578f
NW200602\USER <yao-s@clinks.jp>
2020-08-26 16:56:27 +0900

    refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,44 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: _HomePage(),
-    );
-  }
+void main() {
+  runApp(MyApp());
 }
 
-class _HomePage extends StatelessWidget {
-  const _HomePage({Key key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  MyApp({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const _Inherited(
-      message: '🐶',
-      child: Scaffold(
-        body: Center(
-          child: _Message(),
-        ),
+    return MaterialApp(
+      home: _Inherited(
+        child: _Message(),
       ),
     );
   }
 }
 
-class _Message extends StatelessWidget {
-  const _Message({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      'Message: ${_Inherited.of(context, listen: false).message}',
-      style: const TextStyle(fontSize: 96),
-    );
-  }
-}
-
 class _Inherited extends InheritedWidget {
-  const _Inherited({
+  _Inherited({
     Key key,
-    @required this.message,
     @required Widget child,
   }) : super(key: key, child: child);
 
-  final String message;
+  final String message = 'InheritedMessage';
 
-  static _Inherited of(
-    BuildContext context, {
-    @required bool listen,
-  }) {
-    return listen
-        ? context.dependOnInheritedWidgetOfExactType<_Inherited>()
-        : context.getElementForInheritedWidgetOfExactType<_Inherited>().widget
-            as _Inherited;
+  static _Inherited of(BuildContext context) {
+    return context.getElementForInheritedWidgetOfExactType<_Inherited>().widget
+        as _Inherited;
   }
 
   @override
   bool updateShouldNotify(_Inherited old) => false;
+}
+
+class _Message extends StatelessWidget {
+  _Message({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('${_Inherited.of(context).message}');
+  }
 }
