cd704e1c064df4dd9dc153bedd70a72c445a86bf
NW200602\USER <yao-s@clinks.jp>
2020-08-31 16:05:28 +0900

    4-2-1 future sample

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:http/http.dart' as http;
 
 void main() => runApp(MyApp());
 
@@ -19,31 +18,16 @@ class MyApp extends StatelessWidget {
 class _Notifier extends ValueNotifier<String> {
   _Notifier() : super("");
 
-  List<String> listdata = [];
-  List<String> getData() => listdata;
+  int _counter = 0;
 
-  void httpGet() {
-    const url = 'https://jsonplaceholder.typicode.com/posts';
-    print("httpGet called. /url:$url");
-    http.get(url).then((response) {
-      print("Response status: ${response.statusCode}");
-      print("Response body: ${response.body}");
-      listdata = response.body.split("\n");
-      value = response.body;
-    });
+  void action() async {
+    value = await getAsyncString();
   }
 
-  void httpPut() {
-    const url = 'https://httpbin.org/put';
-    print("httpPut called. /url:$url");
-    Map<String, String> headers = {'content-type': 'application/json'};
-    String body = '{"name":"FlutterSample"}';
-    http.put(url, headers: headers, body: body).then((response) {
-      print("Response status: ${response.statusCode}");
-      print("Response body: ${response.body}");
-      listdata = response.body.split("\n");
-      value = response.body;
-    });
+  Future<String> getAsyncString() async {
+    var st = DateTime.now().millisecondsSinceEpoch;
+    await Future.delayed(new Duration(seconds: 2));
+    return "getAsyncString() called. ${++_counter} times. /delay:${DateTime.now().millisecondsSinceEpoch - st}ms";
   }
 }
 
@@ -56,48 +40,24 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Flutter HTTP in Dart sample.")),
+      appBar: AppBar(title: Text("Flutter Future sample.")),
       body: Column(
         children: [
           Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               Expanded(
                 child: Padding(
                   padding: EdgeInsets.all(8.0),
                   child: RaisedButton(
                     onPressed: () =>
-                        MyHomePage.of(context, listen: false).httpGet(),
-                    child: Text('Get'),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: RaisedButton(
-                    onPressed: () =>
-                        MyHomePage.of(context, listen: false).httpPut(),
-                    child: Text('Put'),
+                        MyHomePage.of(context, listen: false).action(),
+                    child: Text('Action'),
                   ),
                 ),
               ),
             ],
           ),
-          Expanded(
-            child: ListView.builder(
-              scrollDirection: Axis.vertical,
-              shrinkWrap: true,
-              itemCount: MyHomePage.of(context, listen: true).getData().length,
-              itemBuilder: (context, int index) {
-                return Padding(
-                  padding: EdgeInsets.all(4.0),
-                  child: Text(
-                      MyHomePage.of(context, listen: false).getData()[index]),
-                );
-              },
-            ),
-          ),
+          Text("${MyHomePage.of(context).value}")
         ],
       ),
     );
