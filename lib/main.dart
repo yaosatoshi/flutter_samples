aae429cd50bb413b2705565923ba6c6df3d71cb0
NW200602\USER <yao-s@clinks.jp>
2020-09-03 15:03:13 +0900

    4-3-1 JSON encode / decode

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -16,56 +15,121 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class _Notifier extends ValueNotifier<String> {
-  _Notifier() : super("");
+class _MyDataSub2 {
+  final double my2doubleDat;
+  final BigInt my2bintDat;
 
-  void encode() {
-    var data = {
-      {'score': 40},
-      [
-        {'a': 40},
-        {'b': 80},
-        {'c': 100, 'overtime': true, 'special_guest': null}
-      ]
-    };
-    value = jsonEncode(data);
-  }
+  _MyDataSub2(double dd, BigInt bid)
+      : my2doubleDat = dd,
+        my2bintDat = bid;
 
-  void decode() {
-    var jsonString = ''' [{"score":40},{"score":80}] ''';
-    var output = "";
+  String log() =>
+      "_MyDataSub2(/my2doubleDat:$my2doubleDat /my2bintDat:$my2bintDat)";
 
-    List<dynamic> scores = jsonDecode(jsonString);
-    var i = 0;
-    scores.forEach((element) {
-      output += "#${++i} : ${element['score']}\n";
-    });
-    value = output;
-  }
+  Map<String, dynamic> toJson() =>
+      {'my2doubleDat': my2doubleDat, 'my2bintDat': my2bintDat.toString()};
+
+  static _MyDataSub2 fromJson(Map<String, dynamic> json) => _MyDataSub2(
+      json['my2doubleDat'] as double,
+      BigInt.parse(json['my2bintDat'] as String));
 }
 
-class _MyDataSub {
-  String stringDat;
-  _MyDataSub(String sd) {
-    stringDat = sd;
-  }
-  static
+class _MyDataSub1 {
+  final String my1stringDat;
+  final bool my1boolDat;
+
+  _MyDataSub1(String sd, bool bd)
+      : my1stringDat = sd,
+        my1boolDat = bd;
+
+  String log() =>
+      "_MyDataSub1(/my1stringDat:$my1stringDat　/my1boolDat:$my1boolDat)";
+
+  Map<String, dynamic> toJson() =>
+      {'my1stringDat': my1stringDat, 'my1boolDat': my1boolDat};
+
+  static _MyDataSub1 fromJson(Map<String, dynamic> json) =>
+      _MyDataSub1(json['my1stringDat'], json['my1boolDat']);
 }
 
 class _MyData {
-  int intDat;
-  String stringDat;
-  List<int> intarrayDat;
-  List<_MyDataSub> objectarrayDat;
+  final int intDat;
+  final String stringDat;
+  final List<int> intListDat;
+  final List<String> stringListDat;
+  final _MyDataSub1 my1Dat;
+  final List<_MyDataSub2> my2ListDat;
 
-  _MyData(int id, String sd, List<int> iad, List<_MyDataSub> oad) {
-    intDat = id;
-    stringDat = sd;
-    intarrayDat = iad;
-    objectarrayDat = oad;
+  _MyData(int id, String sd, List<int> ild, List<String> sld, _MyDataSub1 my1d,
+      List<_MyDataSub2> my2ld)
+      : intDat = id,
+        stringDat = sd,
+        intListDat = ild,
+        stringListDat = sld,
+        my1Dat = my1d,
+        my2ListDat = my2ld;
+
+  String log() {
+    String my2ListLog = "[";
+    my2ListDat.map((item) => my2ListLog += "${item.log()},");
+    my2ListLog += "]";
+    return "_MyData(/intDat:$intDat /stringDat:$stringDat /intListDat:$intListDat /stringListDat:$stringListDat /my1Dat:${my1Dat.log()} /my2ListDat:$my2ListLog)";
   }
+
+  Map<String, dynamic> toJson() => {
+        'intDat': intDat,
+        'stringDat': stringDat,
+        'intListDat': intListDat,
+        'stringListDat': stringListDat,
+        'my1Dat': my1Dat.toJson(),
+        'my2ListDat': my2ListDat.map((item) => item.toJson()).toList()
+      };
+
+  String toJsonString() => jsonEncode(toJson());
+
+  static _MyData fromJson(Map<String, dynamic> json) => _MyData(
+      json['intDat'],
+      json['stringDat'],
+      json['intListDat'].cast<int>(),
+      json['stringListDat'].cast<String>(),
+      _MyDataSub1.fromJson(json['my1Dat']),
+      json['my2ListDat']
+          .map((item) => _MyDataSub2.fromJson(item))
+          .toList()
+          .cast<_MyDataSub2>());
+
+  static _MyData fromJsonString(String jsonString) =>
+      fromJson(jsonDecode(jsonString));
 }
 
+class _Notifier extends ValueNotifier<String> {
+  final _MyData sampleData = _MyData(
+      500,
+      "STRING",
+      [0, 1, 2, 3, 4],
+      ["A", "B", "C"],
+      _MyDataSub1("Sub1", true),
+      [
+        _MyDataSub2(0.7, BigInt.from(4000)),
+        _MyDataSub2(1.4, BigInt.from(8000)),
+        _MyDataSub2(2.1, BigInt.from(12000))
+      ]);
+
+  //String get sampleJsonString => sampleData.toJsonString();
+  String get sampleJsonString => '{"intDat":500,"stringDat":"STRING","intListDat":[0,1,2,3,4],"stringListDat":["A","B","C"],"my1Dat":{"my1stringDat":"Sub1","my1boolDat":true},"my2ListDat":[{"my2doubleDat":0.7,"my2bintDat":"4000"},{"my2doubleDat":1.4,"my2bintDat":"8000"},{"my2doubleDat":2.1,"my2bintDat":"12000"}]}';
+
+  _Notifier() : super("");
+
+  // _MyData to jsonString.
+  void encode() {
+    value = sampleData.toJsonString();
+  }
+
+  // jsonString to _MyData.
+  void decode() {
+    value = _MyData.fromJsonString(sampleJsonString).log();
+  }
+}
 
 class MyHomePage extends StatelessWidget {
   MyHomePage({Key key}) : super(key: key);
@@ -104,10 +168,9 @@ class MyHomePage extends StatelessWidget {
               ),
             ],
           ),
-          Expanded(
-            child: Text(MyHomePage
-                .of(context, listen: true)
-                .value),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(MyHomePage.of(context, listen: true).value),
           ),
         ],
       ),
