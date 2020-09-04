955aa580cb4c4edaba95e79caf8e68250c8f1a8a
NW200602\USER <yao-s@clinks.jp>
2020-09-04 16:25:59 +0900

    4-5-1 AppLifecycleState

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:http/http.dart' as http;
 
 void main() => runApp(MyApp());
 
@@ -10,96 +8,49 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: ChangeNotifierProvider(
-          create: (context) => _Notifier(), child: MyHomePage()),
+      home: MyHomePage(),
     );
   }
 }
 
-class _Notifier extends ValueNotifier<String> {
-  _Notifier() : super("");
-
-  List<String> listdata = [];
-  List<String> getData() => listdata;
-
-  void httpGet() {
-    const url = 'https://jsonplaceholder.typicode.com/posts';
-    print("httpGet called. /url:$url");
-    http.get(url).then((response) {
-      print("Response status: ${response.statusCode}");
-      print("Response body: ${response.body}");
-      listdata = response.body.split("\n");
-      value = response.body;
-    });
-  }
-
-  void httpPut() {
-    const url = 'https://httpbin.org/put';
-    print("httpPut called. /url:$url");
-    Map<String, String> headers = {'content-type': 'application/json'};
-    String body = '{"name":"FlutterSample"}';
-    http.put(url, headers: headers, body: body).then((response) {
-      print("Response status: ${response.statusCode}");
-      print("Response body: ${response.body}");
-      listdata = response.body.split("\n");
-      value = response.body;
-    });
-  }
-}
-
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   MyHomePage({Key key}) : super(key: key);
 
-  static _Notifier of(BuildContext context, {bool listen = true}) =>
-      Provider.of<_Notifier>(context, listen: listen);
+  @override
+  State<StatefulWidget> createState() => MyHomePageState();
+}
+
+class MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
+  var _stateString = "(none)";
+
+  void setStateString(String str) {
+    this.setState(() => _stateString = str);
+  }
+
+  @override
+  void initState() {
+    print("initState() called.");
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    print("dispose() called.");
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    print("didChangeAppLifecycleState() called. /state:${state}");
+    setStateString('${state}');
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Flutter HTTP in Dart sample.")),
-      body: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Expanded(
-                child: Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: RaisedButton(
-                    onPressed: () =>
-                        MyHomePage.of(context, listen: false).httpGet(),
-                    child: Text('Get'),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: RaisedButton(
-                    onPressed: () =>
-                        MyHomePage.of(context, listen: false).httpPut(),
-                    child: Text('Put'),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          Expanded(
-            child: ListView.builder(
-              scrollDirection: Axis.vertical,
-              shrinkWrap: true,
-              itemCount: MyHomePage.of(context, listen: true).getData().length,
-              itemBuilder: (context, int index) {
-                return Padding(
-                  padding: EdgeInsets.all(4.0),
-                  child: Text(
-                      MyHomePage.of(context, listen: false).getData()[index]),
-                );
-              },
-            ),
-          ),
-        ],
-      ),
-    );
+        appBar: AppBar(title: Text("Flutter ApplifecycleState sample.")),
+        body: Center(child: Text(_stateString)));
   }
 }
