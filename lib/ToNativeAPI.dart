3b57bd458ea59d179a874af6e8aafdd0a82d980c
NW200602\USER <yao-s@clinks.jp>
2020-09-08 15:14:22 +0900

    minor fix

--- lib/ToNativeAPI.dart
+++ lib/ToNativeAPI.dart
@@ -5,25 +5,25 @@ class ToNativeAPI {
       "com.example.flutter_application/flutter_to_native_sample");
 
   Future<void> voidMethodNoParam() async {
-    await _channel.invokeMethod('voidMethodNoParam');
+    await _channel.invokeMethod<void>('voidMethodNoParam');
   }
 
   Future<int> getIntMethodNoParam() async {
-    return await _channel.invokeMethod('getIntMethodNoParam');
+    return await _channel.invokeMethod<int>('getIntMethodNoParam');
   }
 
   Future<String> getStringMethodParamsInt(int param1) async {
-    return await _channel.invokeMethod('getStringMethodParamsInt', param1);
+    return await _channel.invokeMethod<String>('getStringMethodParamsInt', param1);
   }
 
   Future<List<int>> getIntArrayMethodParamsStringInt(
       String param1, int param2) async {
     final Map param = <String, dynamic>{'param1': param1, 'param2': param2};
-    return await _channel.invokeListMethod(
+    return await _channel.invokeListMethod<int>(
         'getIntArrayMethodParamsStringInt', param);
   }
 
   Future<Map<String, dynamic>> getMapMethodNoParams() async {
-    return await _channel.invokeMapMethod('getMapMethodNoParams');
+    return await _channel.invokeMapMethod<String, dynamic>('getMapMethodNoParams');
   }
 }
