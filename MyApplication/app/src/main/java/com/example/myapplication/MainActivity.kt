e6a944e58dffdbf761afd3bec1f824645a7f935f
NW200602\USER <yao-s@clinks.jp>
2020-09-16 18:19:04 +0900

    8-4 cached flutter engine

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.engine.FlutterEngineCache
+import io.flutter.embedding.engine.dart.DartExecutor
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -12,8 +15,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val flutterEngine = FlutterEngine (this)
+        flutterEngine.dartExecutor.executeDartEntrypoint(DartExecutor.DartEntrypoint.createDefault())
+        FlutterEngineCache.getInstance().put("MY_ENGINE_ID", flutterEngine)
+
         button.setOnClickListener {
-            startActivity(Intent(this, FlutterActivity::class.java))
+            startActivity(FlutterActivity.withCachedEngine("MY_ENGINE_ID").build(this))
         }
     }
 }
